99a65652102f226eba428f64569fb1adf2ed5fff
Md. Sohan Millat Sakib <millatsakib01@gmail.com>
2024-05-13 12:54:19 +0600

    Add Query Functionality completed

--- index.js
+++ index.js
@@ -4,20 +4,24 @@ const app = express();
 const jwt = require('jsonwebtoken');
 const cookieParser = require('cookie-parser');
 const port = process.env.PORT || 5000;
+const bodyParser = require('body-parser');      //add body parser for hassel free to conver the req body into josn
 require('dotenv').config()
 
 //middleware
 
-app.use(cors({
-    origin: ["http://localhost:5173/", "http://localhost:5174/"],
-    credentials: true,
-}))
-app.use(express.json());
+// app.use(cors({
+//     origin: ["http://localhost:5173/", "http://localhost:5174/"],
+//     credentials: true,
+// }))
+// app.use(express.json());
 
-app.get('/', (req, res) => {
-    res.send("Doctor is running");
+// app.get('/', (req, res) => {
+//     res.send("Thanks for reach out our biKolpo Server");
 
-})
+// })
+app.use(cors())
+app.use(bodyParser.json());
+
 
 
 
@@ -40,68 +44,42 @@ async function run() {
     try {
         // Connect the client to the server	(optional starting in v4.7)
         // await client.connect();
-        const serviceCollection = client.db('carDoctor').collection('services')
-        const bookingCollection = client.db('carDoctor').collection("bookingCollection")
+        const recomendationCollection = client.db('biKolpo').collection('recomendation')
+        const queriesCollection = client.db('biKolpo').collection("queries")
 
         //auth related api
 
 
-        app.post('/jwt', async (req, res) => {
-            const user = req.body;
-            const token = jwt.sign(user, process.env.Access_Token_Secret, { expiresIn: '1h' })
-            res.cookie('token', token, {
-                httpOnly: true,
-                secure: false,  //If it is https then you have to set securre : ture
-                sameSite: 'none'
-            }).send({ success: true });
+        // app.post('/jwt', async (req, res) => {
+        //     const user = req.body;
+        //     const token = jwt.sign(user, process.env.Access_Token_Secret, { expiresIn: '1h' })
+        //     res.cookie('token', token, {
+        //         httpOnly: true,
+        //         secure: false,  //If it is https then you have to set securre : ture
+        //         sameSite: 'none'
+        //     }).send({ success: true });
 
-        })
-
-
-        //service related api
-
-
-        app.get("/services", async (req, res) => {
-            const cursor = serviceCollection.find();
-            const result = await cursor.toArray();
-            res.send(result);
-        })
-
-        app.get("/service/:id", async (req, res) => {
-            const id = req.params.id;
-            console.log(id);
-            const query = { _id: new ObjectId(id) }
-            const options = {
-                projection: {
-                    title: 1,
-                    price: 1,
-                    service_id: 1
-                },
+        // })
+        app.post("/addQueries", async (req, res) => {
+            try {
+                const data = req.body;
+                const result = await queriesCollection.insertOne(data)
+                if (result.acknowledged === true) {
+                    res.status(201).send("Query Added Successfully!")
+                }
+                else {
+                    res.send("")
+                }
+            }
+            catch {
+                res.send("Database connection error!");
             }
 
-            const result = await serviceCollection.findOne(query, options);
-            res.send(result)
-            console.log(result);
+
         })
 
 
-        app.post("/bookings", async (req, res) => {
-            const booking = req.body;
-            const result = await bookingCollection.insertOne(booking);
 
-            res.send(result);
-        })
-
-        app.get('/bookings', async (req, res) => {
-
-            let query = {};
-            // res.send(req.query.email)
-            if (req.query?.email) {
-                query = { email: req.query.email }
-                const result = await bookingCollection.find(query).toArray();
-                res.send(result);
-            }
-        })
 
     } finally {
 
@@ -111,5 +89,5 @@ run().catch(console.dir);
 
 
 app.listen(port, () => {
-    console.log("Car Doctor are running...");
+    console.log("biKolpo are Running on PORT:...", port);
 })
